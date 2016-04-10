f816a40ebf2eea62d8854f431b0433ebcde44ee3
itaydafna <itaydafna@gmail.com>
2016-04-10 23:35:14 +0300

    slider working on click

---
+++ script.js
@@ -0,0 +1,20 @@
+function togglePic() {
+  var innerSlider = document.getElementById('beatles'),
+  	  sliderStyle = window.getComputedStyle(innerSlider),
+  	  leftMargin =sliderStyle.getPropertyValue("margin-left");
+  if (leftMargin === "0px") {
+    innerSlider.style.marginLeft = "-300px";
+  }
+  else if(leftMargin === "-300px"){
+  	innerSlider.style.marginLeft = "-600px"
+  }
+  else if(leftMargin === "-600px"){
+  	innerSlider.style.marginLeft = "-900px"
+  }
+  else if(leftMargin==="-900px"){
+  	innerSlider.style.marginLeft = "0px"
+  }
+
+};
+
+document.getElementById('beatles').addEventListener("click",togglePic);
